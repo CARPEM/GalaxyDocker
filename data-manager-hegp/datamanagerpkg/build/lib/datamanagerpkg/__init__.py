111382f2904747d5ca3d42b40f91e7a2c55fd709
williamdigan <william.digan.er@gmail.com>
2017-06-07 15:37:14 +0200

    correct the no module issue due to the build folder in the datamanagerpkg folder

--- data-manager-hegp/datamanagerpkg/build/lib/datamanagerpkg/__init__.py
+++
@@ -1,7 +0,0 @@
-import GalaxyCommunication_data_manager
-import ProtonCommunication_data_manager
-from bioblend.galaxy import GalaxyInstance
-
-
-def grou():
-    return ("Artung, Artung, shnell shnell")
